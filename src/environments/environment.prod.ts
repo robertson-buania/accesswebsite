8497d2542b095cab08fe4a9924e08d224d541609
buania <robertsonbilonda@hotmail.com>
2022-06-25 11:21:41 +0100

    site s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  urlServeur:"https://www.dashkay.academy.opengroupinnovation.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   //urlServeur:"http://localhost:8181/website/kay"
-  urlServeur:"http://www.dashkay.academy.opengroupinnovation.com"
-};
+  urlServeur:"https://www.dashkay.academy.opengroupinnovation.com"
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
